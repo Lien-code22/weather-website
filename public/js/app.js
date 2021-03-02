d1abc004e127f5977008eb10821c78ff3c20cb4f
Neil Arteza <neil.arteza@training>
2021-03-02 11:44:37 +0800

    Set app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ const location = search.value
 messageOne.textContent = 'loading...'
 messageTwo.textContent =''
 
-fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
+fetch('/weather?address='+ location).then((response)=>{
     response.json().then((data)=>{
         console.log(data)
         if(data.error){
