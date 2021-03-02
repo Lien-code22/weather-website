4fe0105aa2cb65abe4d2a8f4baf3bf4e105a4da1
Neil Arteza <neil.arteza@training>
2021-03-02 10:35:04 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,15 @@
+const request = require('request')
+
+const forecast =(latitute,longtitude,callback)=>{
+    const url ='http://api.weatherstack.com//current?access_key=0f3543efa0991be038bff4dad9b44f85&query='+ longtitude +','+ latitute + '&units=f'
+    request ({url, json:true},(error,{body})=>{
+        if(error){
+            callback('Unable to connect to weather service',undefined)
+        }else if(body.error){
+            callback('Unable to find location',undefined)
+        }else{
+            callback(undefined,`${body.current.weather_descriptions[0]} .It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+        }
+    })
+}
+module.exports =forecast
